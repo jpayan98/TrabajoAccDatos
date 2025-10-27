882e519bd5e9aa6a71867a4967e69d851a2a6efb
jpayan98 <jpayan98@gmail.com>
2025-10-27 19:14:13 +0100

    modificacion1

--- UsoDB.py
+++ UsoDB.py
@@ -2,9 +2,24 @@ import sqlite3
 import libsql
 import envyte
 
-url = envyte.get("URL_DB")
-auth_token = envyte.get("API_TOKEN")
+print("=== SELECCIONA MODO DE CONEXIÓN ===")
+print("1. Base de datos local (SQLite)")
+print("2. Base de datos en la nube (Turso)")
+opcion = input("Opción: ")
 
-# Conexión con Turso
-conn = libsql.connect("aadut1", sync_url=url, auth_token=auth_token)
-conn.sync()
+match opcion:
+    case "1":
+        conn = sqlite3.connect("tienda.db")
+        print("💾 Conectado a base de datos local SQLite.")
+        PyC=";"
+    case "2":
+        url = envyte.get("URL_DB")
+        auth_token = envyte.get("API_TOKEN")
+        conn = libsql.connect("aadut1", sync_url=url, auth_token=auth_token)
+        PyC=""
+        conn.sync()
+        print("🌐 Conectado a base de datos Turso.")
+    case _:
+        print("❌ Opción no válida. Usando SQLite por defecto.")
+        conn = sqlite3.connect("tienda.db")
+        
