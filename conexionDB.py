2dceb07643a92e49f39c515b3c9243cda1938754
Jesús Ramírez <jesusramirez1627@gmail.com>
2025-10-28 13:43:22 +0100

    Modificacion

--- conexionDB.py
+++
@@ -1,8 +0,0 @@
-import libsql
-import envyte
-
-"""Crea y devuelve la conexión con Turso."""
-url = envyte.get("URL_DB")
-auth_token = envyte.get("API_TOKEN")
-conn = libsql.connect("aadut1", sync_url=url, auth_token=auth_token)
-conn.sync()
